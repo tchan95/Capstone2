3fe5902b46a77d531556b8a2736b62a795a6b121
pyo yong chan <tchan95@naver.com>
2018-05-17 20:37:34 +0900

    hongin's
    scene

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    
+    public void click()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void ClickFrom3D()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
