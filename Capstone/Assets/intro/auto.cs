5de1a489d05421b1d8f0e7b724580fb5722b2848
Eumhongin <zizon0wpxm@naver.com>
2018-06-03 00:37:46 +0900

    배경수정
    해상도 고정. 16:9(1600*900)

---
+++ Capstone/Assets/intro/auto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class auto : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(1600, 900, true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
